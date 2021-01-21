ffeca3e4b9489da2e024ee28508e8ba08f7d8458
dhavalmaniyar <logoutdhaval@gmail.com>
2021-01-21 11:10:01 +0530

    migrations and admin page added

--- coremetric/settings.py
+++ coremetric/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'coremetric_app.apps.CoremetricAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- coremetric_app/views.py
+++ coremetric_app/views.py
@@ -1,5 +1,15 @@
+# from django.core.checks import messages
 from django.shortcuts import render
-
+from .models import Comment
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+    
+def submit(request):
+    rname=request.POST['name']
+    rcomment=request.POST['comment']
+    remail=request.POST['email']
+    submitData=Comment(name=rname,comment=rcomment,email=remail)
+    submitData.save()
+    return render(request,'index.html',{'message':'Form submitted successfully'})
+   
