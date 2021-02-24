29f647f1bfb60ebd376d6c9ea8785e407c14aa9f
dhavalmaniyar <logoutdhaval@gmail.com>
2021-02-24 18:38:05 +0530

    jfsk

--- coremetric/urls.py
+++ coremetric/urls.py
@@ -13,11 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin, sitemaps
 from django.urls import path, include
-
-
+from coremetric_app.sitemaps import *
+from django.contrib.sitemaps.views import sitemap 
+sitemaps={
+    'index':StaticViewSitemapIndex
+}
 urlpatterns = [
     path('', include('coremetric_app.urls')),
     path('admin/', admin.site.urls),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
 ]
--- coremetric_app/urls.py
+++ coremetric_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include,path
 from . import views
+from django.views.generic import TemplateView
 urlpatterns = [
     path('',views.index,name='index'),
     path('submit',views.submit,name='submit'),
+    path('robots.txt',TemplateView.as_view(template_name='robots.txt', content_type="text/plain"),),
 ]
